7e1c5ee193d646c2a7260936d7ab07509c163585
Develooser <tinafeixas@gmail.com>
2021-09-22 18:03:45 +0200

    22091803

--- app-1.php
+++ app-1.php
@@ -27,17 +27,36 @@ números que al darle click lleven a esos enlaces.
         }*/
         //var_dump(variable) -> sirve para que te muestre que tipo de dato es, y que contiene esa variable.
 
+        
 
         $links[0]['url'] = "https://github.com";
         $links[1]['url'] = "https://www.php.net";
+        $links[2]['url'] = "https://www.w3schools.com/";
 
         $links[0]['img'] = "git.jpeg";
         $links[1]['img'] = "php.png";
+        $links[2]['img'] = "W3S.png";
 
-        foreach ($links as $link){        
+
+        if(isset($_GET['id'])){
+            if($_GET['id'] <= (count($links)-1)){
+                echo '<a href="' . $links[$_GET['id']]['url'] . '"><img src="' .$links[$_GET['id']]['img']. '"  width="100">  </img></a>'; 
+            } else {
+                foreach ($links as $link){        
+                    echo '<a href="' . $link['url'] . '"><img src="' .$link['img']. '"  width="100">  </img></a>';                  
+                }
+            } 
+        } else {
+            foreach ($links as $link){        
+                echo '<a href="' . $link['url'] . '"><img src="' .$link['img']. '"  width="100">  </img></a>';                 
+            }
+        }
+
+
+       /* foreach ($links as $link){        
            echo '<a href="' . $link['url'] . '"><img src="' .$link['img']. '"  width="100">  </img></a><br>'; 
             
-        }
+        }*/
 
     ?>
 

fd943199aa89fe64cba061dada70b50bbd9dde55
Develooser <tinafeixas@gmail.com>
2021-09-23 21:34:23 +0200

    23092134

--- app-1.php
+++ app-1.php
@@ -14,20 +14,40 @@ números que al darle click lleven a esos enlaces.
 </head>
 <body>
     <?php
-        /*$links = array("https://github.com", "https://www.php.net");
-        $images = array("/img/git.jpeg", "/img/php.png");
-        $i = 0;
-
-        for ($i=0; $i<count($links);$i++){
-            echo '<a href="' . $links[$i] . '">' . ($i+1) . '</a><br>';
-        }*/
-
-        /*foreach ($links as $link){
-            echo '<a href="' . $link . '">' . $i++ . '</a><br>'; 
-        }*/
+       
         //var_dump(variable) -> sirve para que te muestre que tipo de dato es, y que contiene esa variable.
 
-        
+        $usuarios[0]['username'] = "user";
+        $usuarios[0]['password'] = "0123";
+        $usuarios[1]['username'] = "user1";
+        $usuarios[1]['password'] = "1234";
+        $usuarios[2]['username'] = "user2";
+        $usuarios[2]['password'] = "abcd";
+        $identificador = 0;
+
+
+        if (isset($_POST['user'])){
+            foreach ($usuarios as $usuario){
+                if ($usuario['username'] == $_POST['user']){
+                    $identificador = 1;
+                    if(isset($_POST['password'])){
+                        if ($usuario['password'] == $_POST['password']){
+                            echo "<p>Se ha iniciado sesión correctamente</p>";
+                    
+                        } else {
+                            echo "<p>Contraseña incorrecta</p>";
+                        }
+                    }                
+                }
+            }
+        }
+
+        if ($identificador == 0){
+            echo "<p>No existe el usuario</p>";
+        }
+
+
+        //IMAGENES Y ENLACES GET
 
         $links[0]['url'] = "https://github.com";
         $links[1]['url'] = "https://www.php.net";
@@ -50,14 +70,33 @@ números que al darle click lleven a esos enlaces.
             }
         }
 
-
-       /* foreach ($links as $link){        
-           echo '<a href="' . $link['url'] . '"><img src="' .$link['img']. '"  width="100">  </img></a><br>'; 
-            
-        }*/
-
+        
     ?>
 
+    <p>Formulario con tipo de texto a GET</p>
+    <form method="GET" action="app-1.php">
+        <input type="text" name="id" placeholder= "id"/>
+        <input type="submit"/>
+    </form>
+    <p>Formulario con botones a GET</p>
+    <form method="GET" action="app-1.php">
+        <input type="submit" name="id" value="1"/>
+        <input type="submit" name="id" value="2"/>
+        <input type="submit" name="id" value="3"/>
+    </form>
+
+    <p>Formulario con tipo de texto a POST</p>
+    <form method= "POST" action="app-1.php">
+        <input type="text" name="user" placeholder= "usuario"/>
+        <input type="submit"/>
+    </form>
+
+    <p>Comprobar usuario y contraseña</p>
+    <form method= "POST" action="app-1.php">
+        <input type="text" name="user" placeholder= "usuario"/>
+        <input type="password" name="password" placeholder= "contraseña"/>
+        <input type="submit"/>
+    </form>
 
 </body>
 </html>
